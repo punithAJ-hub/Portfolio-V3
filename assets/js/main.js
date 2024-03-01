d6f5726e356fce2172bd21eb55896d1626c4329b
punithAJ-hub <punithaj0007@gmail.com>
2024-03-01 16:18:22 -0500

    Typed Animaion

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,30 @@ menuLinks.forEach((menuLink)=>{
         navBarMenu.classList.remove('active');
     })
 })
+
+
+/* Change  background header */
+
+function scrollHeader(){
+    const header= document.getElementById('header');
+    this.scrollY>= 20 ? header.classList.add('active') : header.classList.remove('active');
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+/* Hero type effects */
+
+const typed = document.querySelector('.typed');
+
+if(typed){
+    let typed_strings= typed.getAttribute('data-typed-items');
+    typed_strings= typed_strings.split(",");
+
+    new Typed('.typed',{
+        strings: typed_strings,
+        loop:true,
+        typeSpeed : 100,
+        backSpeed:50,
+        backDelay:200
+    })
+}
