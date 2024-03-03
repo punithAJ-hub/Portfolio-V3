6aa771604d3bf285848d9a4231592225fce4f83d
punithAJ-hub <punithaj0007@gmail.com>
2024-03-02 20:22:16 -0500

    Portfolio html part

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,58 @@ if(typed){
         backDelay:2000
     })
 }
+
+
+/*     Scroll Sections Active link   */
+
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive(){
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(section =>{
+        const sectionHeight = section.offsetHeight;
+        const sectionTop = section.offsetTop - 50;
+
+        let sectionId = section.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <=sectionTop + sectionHeight){
+            document.querySelector('.menu a[href *=' + sectionId + ']').classList.add('active_link');
+        }
+        else{
+            document.querySelector('.menu a[ href *=' + sectionId + ']').classList.remove('active_link');
+        }
+    });
+}
+
+window.addEventListener('scroll' , scrollActive);
+
+
+/* Resume Scroll bar */
+
+
+const pages =document.querySelectorAll('.page');
+const resume =document.querySelector('.resume');
+
+
+function resumeActive(){
+
+    const scrollY = window.pageYOffset;
+
+    pages.forEach(page=>{
+
+        const sectionHeight = page.offsetHeight ;
+        const sectionTop = page.offsetTop - 500 ;
+
+        let sectionId = page.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY<= sectionHeight + sectionTop){
+            document.querySelector('.resume_tabs a[href *=' + sectionId + ']').classList.add('current');
+        }
+        else{
+            document.querySelector('.resume_tabs a[href *=' + sectionId + ']').classList.remove('current');
+        }
+    })
+}
+
+window.addEventListener('scroll' , resumeActive);
